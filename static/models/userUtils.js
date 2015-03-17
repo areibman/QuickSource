98a39414e1b7a66976407501d1b779d6ea8a7ba3
Yu-Hsin(Henry) Chen <yu-hsin.chen@emory.edu>
2015-03-17 17:26:43 -0400

    Manual merge to master

---
+++ static/models/userUtils.js
@@ -0,0 +1,25 @@
+/****** Static Database Communication ******/
+//Register & Save
+function db_update(user){
+    //Update user info
+}
+function db_register(user){
+    //Register new user
+    //Set/Return pk
+}
+
+//Getters
+function db_fetch_withPk(pk){
+    //Return user obj
+}
+function db_fetch_withUsername(username){
+    //Return user obj
+}
+
+//Checks
+function db_hasUser(user){
+    //Return true | false
+}
+function db_authenticate(user){
+    //Return true | false
+}
