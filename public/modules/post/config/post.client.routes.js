8e65fcb30ecf396fa53fda0aa68f3ffe540e61fd
nihar.parikh@emory.edu <nihar.parikh@emory.edu>
2015-03-17 15:32:49 -0400

    Added project route

--- public/modules/post/config/post.client.routes.js
+++ public/modules/post/config/post.client.routes.js
@@ -5,6 +5,10 @@ angular.module('post').config(['$stateProvider',
 	function($stateProvider) {
 		// Post state routing
 		$stateProvider.
+		state('project', {
+			url: '/project',
+			templateUrl: 'modules/post/views/project.client.view.html'
+		}).
 		state('create', {
 			url: '/create',
 			templateUrl: 'modules/post/views/create.client.view.html'
