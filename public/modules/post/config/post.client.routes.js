2aec099ff39d44c50f66bcdd8734f0cf9f182dbe
nihar.parikh@emory.edu <nihar.parikh@emory.edu>
2015-03-17 14:37:44 -0400

    Added route for create view

--- public/modules/post/config/post.client.routes.js
+++ public/modules/post/config/post.client.routes.js
@@ -5,6 +5,10 @@ angular.module('post').config(['$stateProvider',
 	function($stateProvider) {
 		// Post state routing
 		$stateProvider.
+		state('create', {
+			url: '/create',
+			templateUrl: 'modules/post/views/create.client.view.html'
+		}).
 		state('browse', {
 			url: '/browse',
 			templateUrl: 'modules/post/views/browse.client.view.html'
