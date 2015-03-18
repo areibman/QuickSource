af98466f9840b65d53ae74935414978aa6e21752
Yu-Hsin(Henry) Chen <yu-hsin.chen@emory.edu>
2015-03-18 17:45:55 -0400

    Repo reset to default

--- app/routes/comment.server.routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-    var comment = require('../../app/controllers/comment.server.controller');
-
-    //Create a comment to a post
-    app.route('comment/:postID/create').post(comment.create);
-};
