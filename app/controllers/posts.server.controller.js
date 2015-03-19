0e4955cf11b13409929595474627c8ae9d98cc5e
Yu-Hsin(Henry) Chen <yu-hsin.chen@emory.edu>
2015-03-19 02:09:25 -0400

    Update post model

--- app/controllers/posts.server.controller.js
+++ app/controllers/posts.server.controller.js
@@ -34,52 +34,23 @@ exports.create = function(req, res) {
  */
 exports.read = function(req, res) {
     Post.findById(req.params.postId, function(err, post){
-        if(!err && post){
-            User.findOne({_id : post.user}).select('displayName roles school isActive').exec(function(err, user){
-                if(err || !user.isActive){
-                   res.status(400).send({ message: 'Post user not found'});
-               }
-               else{
-                    var postObject =
-                    {
-                        _id: post._id,
-                        __v: post.__v,
-                        title: post.title,
-                        abstract: post.abstract,
-                        description: post.description,
-                        location: post.location,
-                        created: post.created,
-                        updated: post.updated,
-                        isActive: post.isActive
-                    };
-
-                    //user
-                    User.findById(post.user).select('_id displayName roles school isActive').exec(function (err, owner) {
-                        postObject.user = owner;
-                        if (err) res.status(400).send({ message: 'Owner user not found'});
-                        //Interested Users
-                        User.find({
-                            '_id': {$in: post.interestedUsers},
-                            'isActive': true
-                        }).select('_id displayName roles school isActive').exec(function (err, interestedUsers) {
-                            postObject.interestedUsers = interestedUsers;
-                            if (err) res.status(400).send({ message: 'Owner user not found'});
-                            //Participants
-                            User.find({
-                                '_id': {$in: post.participants},
-                                'isActive': true
-                            }).select('_id displayName roles school isActive').exec(function (err, participants) {
-                                postObject.participants = participants;
-                                if (err) res.status(400).send({ message: 'Owner user not found'});
-                                res.jsonp(postObject);
-                            });
-                        });
-                    });
-               }
-            });
-        }
+        if(err && !post){ res.status(400).send({ message: 'Post not found'}); }
         else{
-            res.status(400).send({ message: 'Post not found'});
+            var opt = [
+                { path : 'user', model : 'User', select: 'displayName roles school zipCode isActive', match : { isActive : true }},
+                { path : 'interestedUsers', model : 'User', select: 'displayName roles school zipCode isActive', match : { isActive : true }},
+                { path : 'participants', model : 'User', select: 'displayName roles school zipCode isActive', match : { isActive : true }},
+                { path : 'comments', model : 'Comment',  match : { isActive : true }}
+            ];
+
+            Post.populate(post, opt, function(err, post) {
+                if (err || !post.user.isActive) {
+                    res.status(400).send({message: 'Post user not found'});
+                }
+                else {
+                    res.jsonp(post);
+                }
+            });
         }
     });
 };
@@ -127,7 +98,7 @@ exports.delete = function(req, res) {
  * List of Posts
  */
 exports.list = function(req, res) { 
-	Post.find().sort('-created').populate('user', 'displayName').exec(function(err, posts) {
+	Post.find().sort('-created').populate('user', 'displayName school location').exec(function(err, posts) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
