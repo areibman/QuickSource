5357ebe9991dc8cf2d7c3a7644d24b5ecd638674
Yu-Hsin(Henry) Chen <yu-hsin.chen@emory.edu>
2015-04-01 20:50:13 -0400

    Fix syntax error

--- app/routes/filehandler.server.routes.js
+++ app/routes/filehandler.server.routes.js
@@ -4,5 +4,5 @@ module.exports = function(app) {
     var filehandler = require('../../app/controllers/filehandler.server.controller');
 
     app.route('/file/uploadProfilePic').post(filehandler.uploadProfilePic);
-    app.route('/file/uploadProfileResume').post(filehandler.uploadProfileResume;
+    app.route('/file/uploadProfileResume').post(filehandler.uploadProfileResume);
 };
