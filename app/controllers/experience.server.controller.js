f65f9e11b35acded6c2eaca4632db302ed90bd82
Henry Chen <yuhsinchen19940823@gmail.com>
2015-04-12 15:08:26 -0400

    Generate experience controller functiion for model-database handling

--- app/controllers/experience.server.controller.js
+++ app/controllers/experience.server.controller.js
@@ -4,39 +4,127 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    errorHandler = require('./errors.server.controller'),
+    Experience = mongoose.model('Experience');
 
 /**
  * Create a Experience
  */
-exports.create = function(req, res) {
+var create = function(req, res, type) {
+    var experience = new Experience(req.body);
+    experience.user = req.user;
+    experience.updated = Date.now();
+    experience.type = type;
 
+    experience.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(experience);
+        }
+    });
 };
+exports.createPosition = function(req, res) {   create(req, res, 'position'); };
+exports.createEducation = function(req, res) {  create(req, res, 'education'); };
+exports.createCourse = function(req, res) {     create(req, res, 'course'); };
+exports.createPublication = function(req, res) {create(req, res, 'publication'); };
 
 /**
  * Show the current Experience
  */
 exports.read = function(req, res) {
-
+    res.json(req.experience);
 };
 
 /**
  * Update a Experience
  */
 exports.update = function(req, res) {
+    var experience = req.experience;
 
+    experience = _.extend(experience , req.body);
+    experience.updated = Date.now();
+
+    experience.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(experience);
+        }
+    });
 };
 
 /**
  * Delete an Experience
  */
-exports.delete = function(req, res) {
+exports.setInactive = function(req, res) {
+    var experience = req.experience;
+    experience = _.extend(experience , { isActive : false, updated : Date.now() });
 
+    experience.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(experience);
+        }
+    });
 };
 
 /**
- * List of Experiences
+ * Experience middleware
  */
-exports.list = function(req, res) {
+exports.experienceByID = function(req, res, next, id) {
+    Experience.findById(id).exec(function(err, experience) {
+        if (err) return next(err);
+        if (!experience) return next(new Error('Failed to load Experience ' + id));
 
+        switch(experience.type){
+            case 'position':
+                experience.major = undefined;
+                experience.gpa = undefined;
+                experience.courseNumber = undefined;
+                experience.authors = undefined;
+                break;
+
+            case 'education':
+                experience.courseNumber = undefined;
+                experience.authors = undefined;
+                break;
+
+            case 'course':
+                experience.major = undefined;
+                experience.gpa = undefined;
+                experience.authors = undefined;
+                break;
+
+            case 'publication':
+                experience.major = undefined;
+                experience.gpa = undefined;
+                experience.courseNumber = undefined;
+                break;
+
+            default :
+                return next(new Error('The experience ' + id + ' does not have a specified type'));
+        }
+
+        req.experience = experience;
+        next();
+    });
+};
+
+/**
+ * Experience authorization middleware
+ */
+exports.hasAuthorization = function(req, res, next) {
+    if (req.experience.user.id !== req.user.id) {
+        return res.status(403).send('User is not authorized');
+    }
+    next();
 };
